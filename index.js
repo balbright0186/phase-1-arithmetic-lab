d069d987c19ea4f26bcb074ad66776bb7abccf68
Bethany Albright <balbright0186@gmail.com>
2023-04-02 11:07:34 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1=2;
+let num2=31;
+let multiply= (num1*num2);
+    result: (num1 * num2);
 
-// Write your code here
+
+{ let num3 = 8;
+
+    let num4 = 2;
+
+    let mod = 8/2;
+}
+ 
+
+const random=10;
+Math.floor(Math.random() * 10) + 1;
+
+
+
+let max = (5, 10, 15, 20)
